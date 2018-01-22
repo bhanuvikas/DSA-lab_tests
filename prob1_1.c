3c4474dfdbc197912b48926023f70bbb22714b6f
Bhanu Vikas Yaganti <yagantibhanuvikas@gmail.com>
2018-01-22 11:38:10 +0530

    code changed to handle multiple test cases

--- prob1_1.c
+++ prob1_1.c
@@ -15,33 +15,48 @@ int isPerfect(int len)
 int main()
 {
   char str[100], a[100][100];
-  int n,i,j,len,k,p=0;
-  scanf("%s", str);
-  len = strlen(str);
-  k = isPerfect(len);
-  if(k)
+  int n,i,j,l,len,k,p=0,flag=0;
+  scanf("%d", &n);
+  for(l=0;l<n;l++)
   {
-    for(i=0;i<k;i++)
-    {
-      for(j=0;j<k;j++)
-      {
-        a[i][j] = str[p++];
-        //printf("%d ", a[i][j]);
-      }
-    }
-    for(i=0;i<k;i++)
-    {
-      for(j=0;j<k;j++)
-      {
-        if(!(a[i][j]==a[j][i] && a[i][j]==a[k-j-1][k-i-1]))
-        {
-          printf("NO\n");
-          return 0;
-        }
-      }
-    }
-    printf("YES\n");
+  	flag=0;
+  	p=0;
+  	scanf("%s", str);
+  	len = strlen(str);
+  	k = isPerfect(len);
+  	if(k)
+  	{
+    	for(i=0;i<k;i++)
+    	{
+      		for(j=0;j<k;j++)
+      		{
+        		a[i][j] = str[p++];
+        	//printf("%d ", a[i][j]);
+      		}
+    	}
+    	for(i=0;i<k;i++)
+    	{
+    		if(flag)
+    		{
+    			break;
+    		}
+      		for(j=0;j<k;j++)
+      		{
+        		if(!(a[i][j]==a[j][i] && a[i][j]==a[k-j-1][k-i-1]))
+        		{
+          			printf("NO\n");
+          			flag=1;
+         			break;
+        		}
+      		}
+    	}
+    	if(!flag)
+    	{
+    		printf("YES\n");
+    	}
+  	}
+  	else printf("NO\n");
   }
-  else printf("NO\n");
+  
   return 0;
 }
